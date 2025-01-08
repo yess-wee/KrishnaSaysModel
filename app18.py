136fb9923356721a6633acc613dfded4e312ee42
Yashvi Chauhan <146606093+yess-wee@users.noreply.github.com>
2025-01-08 16:12:05 +0530

    Update app18.py

--- app18.py
+++ app18.py
@@ -14,7 +14,7 @@ import random
 
 load_dotenv()
 # os.environ['GOOGLE_API_KEY'] = 'AIzaSyAQmgOq7z-n3yCotriI6-W3wpzIDap6Xqg'
-os.environ['GOOGLE_API_KEY'] = 'AIzaSyAV1opSnseVjUsxmCa2QStiuwgFTVktqdc'
+os.environ['GOOGLE_API_KEY'] = 'AIzaSyCC-s82WIk9pDUDeKJMrXvjPwrIiqPMDWM'
 GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
 genai.configure(api_key=GOOGLE_API_KEY)
 
